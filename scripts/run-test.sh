707810942bcbb0b6b88c22371844f2c4533b036d
Jorge Clavijo <jym272@gmail.com>
2023-06-27 15:40:44 -0300

    feat: npm run test with redis docker compose

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -7,9 +7,9 @@ source "$dir"/exports.sh
 
 echo -e "\e[1;32mCreating services:\e[0m"
 docker compose -f "$dir"/docker-compose.test.yml up -d
-# if CI is set, then we are running in a CI environment, and exe npm run test:report insted
+
 if [ -z ${CI+x} ]; then
-  npm run test
+  npx jest --runInBand --detectOpenHandles --forceExit
 else
   npm run test:report
 fi
